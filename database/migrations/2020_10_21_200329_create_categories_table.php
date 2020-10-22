cc3ef584098bbf021374ddfdda0aeed3dd26f911
habborge <borgeh@uninorte.edu.co>
2020-10-21 22:16:01 -0500

    changing

---
+++ database/migrations/2020_10_21_200329_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('code')->unique();
+            $table->string('name', 50);
+            $table->text('description');
+            $table->bigInteger('father_id')->index();
+            $table->integer('status');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::patch('update-cart', 'ProductController@update');
 Route::post('add-to-cart-quantity', 'ProductController@addToCartQuantity');
 Route::delete('remove-from-cart', 'ProductController@remove');
 Route::get('/result', 'searchController@searchProducts');
+
 Route::group([
     'middleware' => ['auth']
 ], function() {
