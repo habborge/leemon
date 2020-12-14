9ed9ac653117cbb441ea780007de30137bdee6d0
habborge <borgeh@uninorte.edu.co>
2020-12-14 16:52:42 -0500

    corn job

--- app/Console/Commands/VerifyZonaPagos.php
+++ app/Console/Commands/VerifyZonaPagos.php
@@ -75,10 +75,12 @@ class VerifyZonaPagos extends Command
                     payment_error::insertarError($response);
                 }
             }
+
             $rs = new payment_error();
             $rs->reference_code = "fdfdfd";
             $rs->order_id = 12;
             $rs->description = $info;
+            $rs->save();
         }
         //return 0;
     }
