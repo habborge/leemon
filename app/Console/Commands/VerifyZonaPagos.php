92bae191437fa4153e4c353ddee2e0ba31770ad4
habborge <borgeh@uninorte.edu.co>
2020-12-14 18:52:26 -0500

    corn job

--- app/Console/Commands/VerifyZonaPagos.php
+++ app/Console/Commands/VerifyZonaPagos.php
@@ -41,49 +41,45 @@ class VerifyZonaPagos extends Command
     public function handle()
     {
         $orders = Order::where('status', 'Open')->get();
+        $info = "";
+        foreach ($orders as $order) {
+            $data = [
+                    "int_id_comercio" => 30364,
+                    "str_usr_comercio" => "Leemon",
+                    "str_pwd_comercio" => env('ZV_PA'),
+                    "str_id_pago" => "100498-".$order->id,
+                    "int_no_pago" => 1
+            ];
 
-        // foreach ($orders as $order) {
-        //     $data = [
-        //             "int_id_comercio" => 30364,
-        //             "str_usuario" => "Leemon",
-        //             "str_clave" => env('ZV_PA'),
-        //             "str_id_pago" => "100498-".$order->id,
-        //             "int_no_pago" => 1
-        //     ];
+            $response = Http::post('https://www.zonapagos.com/Apis_CicloPago/api/VerificacionPago', $data);
 
-        //     $response = Http::post('https://www.zonapagos.com/Apis_CicloPago/api/VerificacionPago', $data);
+            if ($response->json()['int_estado'] == 1){
+                if ($response->json()['int_estado'] == 0){
+                    if ($response->json()['int_cantidad_pagos'] == 1){
+                        $info = $response->json()['str_res_pago'];
+                    }else if ($response->json()['int_cantidad_pagos'] > 1){
+                        $info = explode(";", $response->json()['str_res_pago']);
+                    }
+                    // {
+                    //     "int_estado": 1,
+                    //     "int_error": 0,
+                    //     "str_detalle": null,
+                    //     "int_cantidad_pagos": 2,
+                    //     "str_res_pago": "|3772 | | 200 | 1002 | | 12500 | | | 123456789 | Cristina | Vargas | 319632555648 | soporte9@zonavirtual.com | opcion 11 | opcion 12 | opcion 13 | | | | | ; 31 | 3773 | 1 | 1 | 1 | 12500 | 12500 | 13 | camisa | 123456789 | Cristina | Vargas | 319632555648 | soporte9@zonavirtual.com | opcion 11 | opcion 12 | opcion 13 | | | 9/11/2018 12:58:41 PM | 29 | 18092100031 | 2701 | 1022 | BANCO UNION COLOMBIANO | 1468228 | 3 |;"
+                    //   }
+                }else{
+                   // payment_error::insertarError($response);
+                }
+            }
 
-        //     if ($response->json()['int_estado'] == 1){
-        //         if ($response->json()['int_estado'] == 0){
-        //             if ($response->json()['int_cantidad_pagos'] == 1){
-        //                 $info = $response->json()['str_res_pago'];
-
-                       
-
-        //             }else if ($response->json()['int_cantidad_pagos'] > 1){
-        //                 $info = explode(";", $response->json()['str_res_pago']);
-
-        //             }
-        //             // {
-        //             //     "int_estado": 1,
-        //             //     "int_error": 0,
-        //             //     "str_detalle": null,
-        //             //     "int_cantidad_pagos": 2,
-        //             //     "str_res_pago": "|3772 | | 200 | 1002 | | 12500 | | | 123456789 | Cristina | Vargas | 319632555648 | soporte9@zonavirtual.com | opcion 11 | opcion 12 | opcion 13 | | | | | ; 31 | 3773 | 1 | 1 | 1 | 12500 | 12500 | 13 | camisa | 123456789 | Cristina | Vargas | 319632555648 | soporte9@zonavirtual.com | opcion 11 | opcion 12 | opcion 13 | | | 9/11/2018 12:58:41 PM | 29 | 18092100031 | 2701 | 1022 | BANCO UNION COLOMBIANO | 1468228 | 3 |;"
-        //             //   }
-        //         }else{
-        //             payment_error::insertarError($response);
-        //         }
-        //     }
-
-            
-        // }
-
-        $rs = new payment_error();
-        $rs->reference_code = "fdfdfd";
-        $rs->order_id = 12;
-        $rs->description = $info;
-        $rs->save();
+            $rs = new payment_error();
+            $rs->reference_code = $order->reference_code;
+            $rs->order_id = $order->id;
+            $rs->description = "100498-".$order->id."~".$response;
+            $rs->save(); 
+        }
+        
+        
         //return 0;
     }
 }
