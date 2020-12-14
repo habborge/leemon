255e561779654858f46714cef294ca655514bf28
habborge <borgeh@uninorte.edu.co>
2020-12-14 10:03:39 -0500

    recaptcha

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -44,10 +44,14 @@
       <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
           <img src="img/promo.jpg" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+          <img src="img/promo_1B.jpg" class="d-block w-100" alt="...">
         </div>
         <div class="carousel-item">
           <img src="img/promo2.jpg" class="d-block w-100" alt="...">
