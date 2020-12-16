4c1a3db2d3368f62a82db797454dc85dad5dcc14
habborge <borgeh@uninorte.edu.co>
2020-12-15 19:40:27 -0500

    corn job

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('payment:verify')->everyMinute();
+        $schedule->command('payment:verify')->everyFifteenMinutes();
     }
 
     /**
