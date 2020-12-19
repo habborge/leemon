77309e83a084c5e55411301ef102bf35069969f6
habborge <borgeh@uninorte.edu.co>
2020-12-19 09:12:02 -0500

    email confirmation purchase

--- resources/views/emails/purchase.blade.php
+++ resources/views/emails/purchase.blade.php
@@ -8,22 +8,24 @@ Tu Orden de compra es {{ $order->id}}
 <table>
     <tr>
         <td>
-            <h6>Información de Compra</h6>
+            <h3>Información de Compra</h3>
             <p>
                 Via Pago: {{ $order->payment }}
             </p>
         </td>
     </tr>
 </table>
+<span>
 Estamos a la espera de recibir la confirmación del pago por parte de tu entidad bancaria en un plazo no mayor a 24 horas.
 Una vez confirmado, tu pedido será enviado a nuestro Centro de Distribución para ser preparado. Te enviaremos por correo la notificación cuando vaya en camino a tu domicilio.
-
+</span>
 
 <div class="col-12">
-    <table>
+    <table class="table" style="width: 100%" >
         <thead>
           <tr>
             <th>PRODUCTO</th>
+            <th>DESCRIPCIÓN</th>
             <th>PRECIO</th>
             <th>CANT</th>
             <th>TOTAL</th>
@@ -32,23 +34,24 @@ Una vez confirmado, tu pedido será enviado a nuestro Centro de Distribución pa
         <tbody>
             @foreach ($order_details as $detail)
                 <tr>
-                    <td>{{  $detail->proName }}</td>
-                    <td>{{  $detail->proPrice }}</td>
-                    <td>{{  $detail->cantidad }}</td>
-                    <td>$ {{ $order->amount }}</td>
+                    <td><img src="{{ $detail->picture }}" alt="" style="width: 100px"></td>
+                    <td><p>{{  $detail->proName }}</p></td>
+                    <td><p>{{  $detail->proPrice }}</p></td>
+                    <td><p>{{  $detail->cantidad }}</p></td>
+                    <td><p>$ {{ $order->amount }}</p></td>
                 </tr>
             @endforeach
             <tr>
-                <td colspan="4">SUBTOTAL</td>
-                <td class="total">$ {{ $order->amount }}</td>
+                <td colspan="4"><p>SUBTOTAL</p></td>
+                <td class="total"><p>$ {{ $order->amount }}</p></td>
             </tr>
           <tr>
-            <td colspan="4">ENVIO</td>
-            <td class="total">$ 0</td>
+            <td colspan="4"><p>ENVIO</p></td>
+            <td class="total"><p>$ 0</p></td>
           </tr>
           <tr>
-            <td colspan="4" class="grand total">GRAND TOTAL</td>
-            <td class="grand total">$ {{ $order->amount + 0 }}</td>
+            <td colspan="4" class="grand total"><p>TOTAL</p></td>
+            <td class="grand total"><p>$ {{ $order->amount + 0 }}</p></td>
           </tr>
         </tbody>
       </table>
