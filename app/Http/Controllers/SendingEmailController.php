7d154ada6a6c1701be998bed62852a30a1636985
habborge <borgeh@uninorte.edu.co>
2020-12-19 09:15:49 -0500

    email confirmation purchase

--- app/Http/Controllers/SendingEmailController.php
+++ app/Http/Controllers/SendingEmailController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
-use App\Mail\sendToFriend;
+use App\Mail\SendToFriend;
 use App\Member;
 use Mail;
 use Auth;
