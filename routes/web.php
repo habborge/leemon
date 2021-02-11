a80c288ddc749e93e4e1e70eb0a4855073dbfd9a
habborge <borgeh@uninorte.edu.co>
2021-02-11 10:37:27 -0500

    added tcc api

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return redirect('/home');
-});
-
 // Route::get('/', function () {
-//     return redirect('/giveaway/registry');
+//     return redirect('/home');
 // });
+
+Route::get('/', function () {
+    return redirect('/giveaway/registry');
+});
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/product/{id}', 'ProductController@details')->name('productdetails');
