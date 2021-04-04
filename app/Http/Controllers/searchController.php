2e7bc335e6a50b5f3bac40802e33430745529653
habborge <borgeh@uninorte.edu.co>
2021-04-03 22:38:53 -0500

    improving search

--- app/Http/Controllers/searchController.php
+++ app/Http/Controllers/searchController.php
@@ -9,12 +9,48 @@ class searchController extends Controller
 {
     public function searchProducts(Request $request){
        
-        $products = Product::where('quantity', '>', 0)->where('name', 'LIKE', "$request->search")->orWhere('brand', 'LIKE', "%$request->search%")->orWhere('name', 'LIKE', "%$request->search%")->orderBy('name', 'ASC')->paginate(24);
+        // $products = Product::where('quantity', '>', 0)
+        // ->where('name', '=', "$request->search")
+        // ->orWhere('name', 'LIKE', "%$request->search%")
+        // ->orWhere('brand', 'LIKE', "%$request->search%")
+        // ->orderByRaw("FIELD(name, 'vitamina d')", 'asc')
+        // ->paginate(24);
 
-        // $products = Product::whereRaw("MATCH(name,brand) AGAINST(?)", array("\"$request->search\"")) 
-        // //->orWhereRaw("MATCH(name,brand) AGAINST(? IN BOOLEAN MODE)", array("\"$request->search\""))
+        $products = Product::where('name', '=', "$request->search")
+        ->orWhere('name', 'LIKE', "%$request->search%")
+        ->orWhere('brand', 'LIKE', "%$request->search%")
+        ->orderByRaw("CASE WHEN name = '".$request->search."' THEN 10
+        WHEN name LIKE '".$request->search."%' THEN 8
+        WHEN name LIKE '%".$request->search."%' THEN 6
+        WHEN brand LIKE '%".$request->search."%' THEN 4
+        END DESC")
+        ->orderBy('name', 'asc')
+        ->orderBy('quantity', 'desc')
+        ->paginate(24);
+
         
-        // ->orderBy('quantity', 'DESC')
+        // $products = Product::selectRaw("0 AS score,(CASE WHEN name = '".$request->search."' THEN score + 10
+        // WHEN name LIKE '".$request->search."%' THEN score + 8
+        // WHEN name LIKE '%".$request->search."%' THEN score + 6
+        // WHEN brand LIKE '%".$request->search."%' THEN score + 4
+        // END) AS score")
+        // ->orderBy('score', 'DESC')
+        // ->paginate(24);
+
+        // $products = Product::selectRaw("0 AS score,(CASE WHEN name = '".$request->search."' THEN score + 10
+        // WHEN name LIKE '".$request->search."%' THEN score + 8
+        // WHEN name LIKE '%".$request->search."%' THEN score + 6
+        // WHEN brand LIKE '%".$request->search."%' THEN score + 4
+        // END) AS score")
+        // ->orderBy('score', 'DESC')
+        // ->paginate(24);
+
+        // $products = Product::selectRaw("*,(MATCH(name, brand) AGAINST ('\"".$request->search."\"' IN BOOLEAN MODE) * 10) as relevance, (MATCH(name, brand) AGAINST ('".$request->search."' IN BOOLEAN MODE) * 1.5) As relevance2") 
+        // ->whereRaw("MATCH(name,brand) AGAINST(? IN BOOLEAN MODE) > 0", array("\"$request->search \"")) 
+        // ->orWhereRaw("MATCH(name,brand) AGAINST(? IN BOOLEAN MODE) > 0", array("$request->search"))
+        
+        // ->orderBy('relevance', 'DESC')
+        // ->orderBy('relevance2', 'DESC')
         // ->paginate(24);
 
         //$brand = Product::select('brand', 'brand_id')->groupBy('brand', 'brand_id')->get();
