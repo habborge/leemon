a1e4ef51fe36e610d4ae2edb022e8cfe8ab390ad
habborge <borgeh@uninorte.edu.co>
2021-04-12 18:18:32 -0500

    added faq route and faq view

---
+++ resources/views/terms/faq.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+	<META HTTP-EQUIV="CONTENT-TYPE" CONTENT="text/html; charset=utf-8">
+	<TITLE></TITLE>
+	<META NAME="GENERATOR" CONTENT="Leemon Nutricion">
+	<META NAME="AUTHOR" CONTENT="Leemon Nutricion">
+	<META NAME="CREATED" CONTENT="20210225;182000000000000">
+	<META NAME="CHANGEDBY" CONTENT="Designware">
+	<META NAME="CHANGED" CONTENT="20210225;204200000000000">
+	<META NAME="AppVersion" CONTENT="16.0000">
+	<META NAME="Company" CONTENT="Leemon Nutricion">
+	<META NAME="DocSecurity" CONTENT="0">
+	<META NAME="HyperlinksChanged" CONTENT="false">
+	<META NAME="LinksUpToDate" CONTENT="false">
+	<META NAME="ScaleCrop" CONTENT="false">
+	<META NAME="ShareDoc" CONTENT="false">
+	<STYLE TYPE="text/css">
+	
+		@page { size: 8.5in 11in; margin-left: 1.18in; margin-right: 1.18in; margin-top: 0.98in; margin-bottom: 0.98in }
+		P { margin-bottom: 0.08in; direction: ltr; widows: 2; orphans: 2 }
+		A:link { color: #0563c1; so-language: zxx }
+		.valigner{
+			display: flex;
+   			align-items: center;
+			   
+		}
+		.lead{
+			margin-left: auto;
+			font-family: Verdana, serif;
+		}
+	</STYLE>
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<link href="{{ asset('css/style.css') }}" rel="stylesheet">
+	
+</HEAD>
+<BODY LANG="es-CO" LINK="#0563c1" DIR="LTR">
+	<div class="jumbotron jumbotron-fluid">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-6">
+					<a class="navbar-brand" href="{{ url('/') }}">
+						<img src="/img/logo_leemon_small_black.png" alt="" class="leemonlogo2">
+					</a>
+				
+				</div>
+				<div class="col-md-6 text-right valigner">
+					<p class="lead">Leemon - Documentos</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="container">
+        <div class="row justify-content-center">
+			<div class="col-md-8 mt-5 mb-4">
+				<P ALIGN=CENTER STYLE="margin-bottom: 0.18in; line-height: 100%">
+					<FONT COLOR="#000000"><FONT FACE="Verdana, serif"><FONT SIZE=4>
+						<B>Preguntas Frecuentes</B>
+					</FONT></FONT></FONT>
+				</P>
+				<P STYLE="margin-bottom: 0.18in; line-height: 100%">
+					Compras<br>Devoluciones<br>Envíos
+				</P>
+            </div>
+        </div>
+    </div>
+</BODY>
+</HTML>
